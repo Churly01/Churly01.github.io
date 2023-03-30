95c9905910963d3b3bd4a17be3f9345ac6f5126e
churly <nietopetinalcarlos@gmail.com>
2023-03-30 22:50:28 +0200

    Remove unnecessary stuff

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,5 +1,5 @@
 import {useAuth} from '../contexts/AuthContext';
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
